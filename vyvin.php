9fb90bdaefc582e9e5898fb509e9df7842bf2c0e
Tibor Repček <tiborepcek@gmail.com>
2015-02-04 20:45:34 +0100

    Create vyvin.php

---
+++ vyvin.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+ 
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='sk' lang='sk'> 
+	<head>
+<?php
+require_once "inc.funcs.php";
+echo $head;
+?>
+		
+		<title>Tabuľa @ esslm.sk - vývin</title>
+	</head>
+	
+	<body>
+		<div id='wrap'>
+<?php
+echo $header;
+?>			
+
+			<div id='content'>
+<?php
+echo $menu;
+?>				
+				
+				<div class='right'> 
+					<h2>Vývin projektu</h2>
+					<p><strong>Tabuľa @ esslm.sk</strong> je jednoduchý projekt na uľahčenie vyučovania a učenia sa predmetu Informatika. Kombináciou klasických textových poznámok, obrázkových príkladov a videonávodov online by malo byť vyučovanie a učenie sa rýchlejšie, jednoduchšie a vždy prístupné. Viac sa o tomto projekte dočítate na podstránke <a href='index.php'>O tabuli</a>.</p>
+					<p>Na tejto podstránke budem zaznamenávať postupný vývin obsahu a formy stránok projektu:</p>
+					<ol>
+						<li><strong>september 2009</strong> - projekt spustený s podstránkami O tabuli, Grafika, Prezentácia, PowerPoint a Projekt. Popisné obrázky sú na podstránkach Grafika a PowerPoint.</li>
+						<li><strong>december 2009</strong> - pridané 2 videonávody (PhotoFiltre 6.3.2 - základné úpravy obrázku; imator.com - animovaný obrázok (GIF) jednoducho, rýchlo a zadarmo) na podstránku Grafika. Videonávody sú umiestnené na youtube.com (<a href='http://youtube.com/user/esslmsk'>youtube.com/user/esslmsk</a>).</li>
+						<li><strong>január 2010</strong> - pridaná táto podstránka. Na podsránke Grafika doplnené informácie o výraze &ldquo;Pixel&rdquo; a na podstránke Prezentácia doplnená videoukážka prezentácie (trvá približne hodinu) <a href="http://vimeo.com/4094582">Učíme pre život 2009 - Vladimír Burjan - Keď sa žiaci neučia, treba zmeniť školy</a>.</li>
+						<li><strong>február 2010</strong> - zmena farby odkazov na výraznejšiu/viditeľnejšiu farbu a pridané menu pre sekundu (II.O).</li>
+						<li><strong>september 2010</strong> - pridaná možnosť komentovať cez <a href='http://developers.facebook.com/docs/reference/plugins/comments'>Facebook Comments</a> (dúfam, že niečo ako náhrada za papierové poznámky) a zobraziť/skryť odpoveď na niektoré otázky a úlohy. Postupné pridávanie tém pre triedy III.O (tercia), II.ABP a III.AP.</li>
+						<li><strong>december 2010</strong> - pridané interaktívne kvízy vytvorené v <a href='http://hotpot.uvic.ca/'>Hot Potatoes</a> (napr. <a href='kviz-subor.html'>kvíz o súbore</a>), ktoré slúžia na overenie vedomostí z jednotlivých tém.</li>
+						<li><strong>január 2011</strong> - pridané poznámky pre I.O (prímu). Všetci (okrem III.AP) majú na konci každej témy (okrem projektu) možnosť otestovať svoje vedomosti o téme v interaktívnom kvíze.</li>
+						<li><strong>marec 2011</strong> - pridané <a href='komentare.php'>pravidlá komentovania</a> a <a href='navod.php'>návod</a> na prácu s týmito poznámkami.</li>
+						<li><strong>september 2011</strong> - pribúdajú témy pre kvartu (IV.O) a kvintu (V.O).</li>
+						<li><strong>január 2012</strong> - pridané <a href='html_a_css.php'>ukážky a zobrazenia HTML a CSS kódu</a> (100 bodov pre <a href='http://www.debugmode.com/wink/'>Wink</a> a <a href='http://code.google.com/p/swfobject/'>SWFObject</a>).</li>
+						<li><strong>apríl 2013</strong> - pridaná stránka <a href='amara.php'>Amara.org</a> pre III.ABP ako praktická a užitočná ukážka spojenia všetkých druhov informácií.</li>
+						<li><strong>marec 2014</strong> - pridaná stránka <a href='inkscape.php'>Inkscape</a> ako doplnenie témy grafika a <a href="http://esslm.sk/tabula/projekt-prvaci.php">zatraktívnenie prváckeho projektu</a>.</li>
+					</ol>
+					<p>Ak máte akékoľvek otázky, píšte na <a href='mailto:repcek&#64;esslm.sk'>repcek&#64;esslm.sk</a>.</p>
+					<p style='text-align: right'><a href='http://tiborepcek.com/'>Tibor Repček</a> (Autor projektu)</p>
+				</div><!-- end .right -->
+				
+				<div style='clear: both;'> </div>
+			</div><!-- end #content -->
+
+<?php
+echo $footer;
+?>
+		
+		</div><!-- end #wrap -->
+
+<?php
+echo $googleAnalytics;
+?>
+		
+	</body>
+</html>
